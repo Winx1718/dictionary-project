0b9bba5c1f69d353785be1414e3179fe6f36e6a0
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-07-23 13:28:17 -0500

    added css to container and link to github

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ export default function App() {
       <div className="container">
         <header className="App-header">Dictionary</header>
         <Search />
-        <footer className="text-center">Open-source code by Layna Moore</footer>
+        <footer className="text-center">
+          Open-source{" "}
+          <a
+            href="https://github.com/Winx1718/dictionary-project.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            code
+          </a>{" "}
+          by Layna Moore
+        </footer>
       </div>
     </div>
   );
