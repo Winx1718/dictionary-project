96ef2f58732f3e12cd3f32044082b05c64fb067f
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-07-24 13:28:57 -0500

    edits to synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,6 +2,7 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,14 +4,11 @@ export default function Synonyms(props) {
   console.log(props);
   if (props.synonyms) {
     return (
-      <span>
-        <strong>Synonyms: </strong>
         <ul className="Synonyms">
           {props.synonyms.map(function (synonyms, index) {
             return <li key={index}>{synonyms}</li>;
           })}
         </ul>
-      </span>
     );
   } else {
     return null;
