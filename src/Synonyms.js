f610d29f5f021047c8697bee646fd48287d8e37f
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-07-25 18:05:26 -0500

    added Synonyms sub header and length rule

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,13 +2,18 @@ import React from "react";
 
 export default function Synonyms(props) {
   console.log(props);
-  if (props.synonyms) {
+  if (props.synonyms.length !== 0) {
     return (
+      <div>
+        <h6>
+          <strong>Synonyms:</strong>
+        </h6>
         <ul className="Synonyms">
           {props.synonyms.map(function (synonyms, index) {
             return <li key={index}>{synonyms}</li>;
           })}
         </ul>
+      </div>
     );
   } else {
     return null;
